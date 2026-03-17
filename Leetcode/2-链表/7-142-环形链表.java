a40029be9e5324477d0937296ada45779faf0c1d
sz001020 <2197997730@qq.com>
2026-03-18 00:34:24 +0800

    0318-2

--- Leetcode/2-链表/7-142-环形链表.java
+++ Leetcode/2-链表/7-142-环形链表.java
@@ -2,20 +2,19 @@ public class Solution {
     public ListNode detectCycle (ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        while (fast != null && slow != null) {
-            while (fast == slow) {
-                fast = fast.next.next;
-                slow = slow.next;
-            }
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode index1 = fast;
+                ListNode index2 = head;
 
-            ListNode index1 = fast;
-            ListNode index2 = head;
-
-            while (index1 == index2) {
-                index1 = index1.next;
-                index2 = index2.next;
+                while (index1 == index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
             }
-            return index1;
         }
         return null;
     }
