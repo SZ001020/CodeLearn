de6e62986d840f6b6932f69196f5eebcebb77330
SZ-macAir <2197997730@qq.com>
2026-04-02 09:57:53 +0800

    0402-1

--- Leetcode/3-哈希表/7-15-三数之和.java
+++ Leetcode/3-哈希表/7-15-三数之和.java
@@ -9,6 +9,37 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
+
+        for (int i = 0;i < nums.length ; i++) {
+            if(nums[i] > 0 ) {
+                return result;
+            }
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            HashSet<Integer> set = new HashSet<>();
+            for(int j  = i+1;j<nums.length;j++) {
+                if(j > i +2 && nums[j] == nums[j-1] && nums[j - 1] == nums[j - 2]) {
+                    continue;
+                }
+                int c = -nums[i] - nums[j];
+                if(set.contains(c)) {
+                    result.add(Arrays.asList(nums[i],nums[j],c));
+                    set.remove(c);
+                } else {
+                    set.add(nums[j]);
+                }
+            }
+        }
+        return result;
+    }
+}
+    
+// @lc code=end
+
+public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0;i < nums.length;i++) {
             if (nums[i] > 0) {
                 return result;
@@ -37,7 +68,4 @@ class Solution {
             }
         }
         return result;
-    }
 }
-// @lc code=end
-
